33d02bb739f1efa052955875455b82a2127af998
josedaniel <josdanielcch@gmail.com>
2026-06-05 14:54:25 -0400

    sera este el fin del hombre araña ?

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -22,7 +22,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://127.0.0.1:5173',
-  'http://127.0.0.1:5174'
+  'http://127.0.0.1:5174',
+  'https://rococo-malasada-e1ce07.netlify.app'
 ];
 
 app.use(cors({
