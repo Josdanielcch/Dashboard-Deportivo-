c3b6124e3da519b66becfa5e22615284979db232
josedaniel <josdanielcch@gmail.com>
2026-06-05 15:15:40 -0400

    feat: initialize express server entry point in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
-const app = require('./app'); // Importa la app configurada
+const app = require('./src/app'); // Importa la app configurada
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en puerto ${PORT}`);
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
